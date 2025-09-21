57bd0de354ba60336feade2e7b9cb6f3c41553b5
Khairon Daffa Widandro <dkhairon@gmail.com>
2025-09-21 14:52:42 +0700

    Add all the necessary imports

---
+++ app/src/main/java/com/nomadozen/aniverse/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.nomadozen.aniverse
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nomadozen.aniverse.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private var _binding: ActivityHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/nomadozen/aniverse/ui/home/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.nomadozen.aniverse.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nomadozen.aniverse.R
+import com.nomadozen.aniverse.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+}
